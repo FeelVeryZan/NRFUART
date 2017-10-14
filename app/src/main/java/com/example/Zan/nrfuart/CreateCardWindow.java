75041a635f59e89a45b807e3f8c736b125c8ba7d
nodgd <dcc1234@qq.com>
2017-10-14 16:52:34 +0800

    重写CreateCardWindow的通道选择模块，写死了只能是4通道，从而避免了RecView这个与ScrollView打架的东西

--- app/src/main/java/com/example/Zan/nrfuart/CreateCardWindow.java
+++ app/src/main/java/com/example/Zan/nrfuart/CreateCardWindow.java
@@ -7,22 +7,17 @@ import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by nodgd on 2017/09/17.
@@ -42,27 +37,16 @@ public class CreateCardWindow {
 
     private Context mContext;
     int mCardType = 0;
-    String[] cardTypeStr = {"", "SaveCard", "SendCard", "MonitorCard"};
+    String[] cardTypeStr = {"", "Saver", "Sender", "Monitor"};
 
-    //窗口共享部分
+    //窗口根基部分
     private View mContentView;
     private PopupWindow mPopupWindow;
-    private TextView mWindowTitle;
-    private EditText mTitleEditText;
-    private LinearLayout mSingleSelectLayout;
-    private LinearLayout mMultiSelectLayout;
+    //卡片标题输入框
+    private EditText mTagEditText;
     private View mParameterLayout;
-    private Button mSubmitBtn;
-    private Button mCloseBtn;
-    private List<String> mOptionList;
-    //单选通道部分
-    private RecyclerView mSingleSelectRecView;
-    private LinearLayoutManager mSingleSelectLayoutManager;
-    private CreateCardSingleSelectAdapter mSingleSelectAdapter;
-    //多选通道部分
-    private RecyclerView mMultiSelectRecView;
-    private GridLayoutManager mMultiSelectLayoutManager;
-    private CreateCardMultiSelectAdapter mMultiSelectAdapter;
+    //选通道部分
+    private SelectAdapter mSelectAdapter;
     //参数设置部分
 
     private Button mChooseAllBtn;
@@ -72,36 +56,47 @@ public class CreateCardWindow {
     public CreateCardWindow(Context context, final int cardType) {
         mContext = context;
         mCardType = cardType;
-        //设置弹出窗口的属性
+        //设置弹出窗口的根基属性
         mContentView = LayoutInflater.from(mContext).inflate(R.layout.create_card_window, null);
         mPopupWindow = new PopupWindow(mContentView, GridView.LayoutParams.MATCH_PARENT, GridView.LayoutParams.MATCH_PARENT, true);
         mPopupWindow.setContentView(mContentView);
         mPopupWindow.setBackgroundDrawable(new ColorDrawable(0xDFFFFFFF));
         mPopupWindow.setAnimationStyle(R.style.DeviceWindowAnimation);
-        //共享部分和专享部分
-        initSharedPart();
-        initSelectPart();
+        //共享部分
+        initBasePart();
         initSendCard();
-
-        Log.d(TAG, "cardType = " + cardType);
     }
 
-    //共享部分
-    private void initSharedPart() {
+    //公用部分
+    private void initBasePart() {
         //窗口的标题
-        mWindowTitle = (TextView) mContentView.findViewById(R.id.edit_window_title);
-        try {
-            mWindowTitle.setText("Create a " + cardTypeStr[mCardType]);
-        } catch (IndexOutOfBoundsException e) {
-            mWindowTitle.setText("Create a ????Card");
+        TextView mWindowTitle = (TextView) mContentView.findViewById(R.id.edit_window_title);
+        if (mCardType == MODE_SAVECARD) {
+            mWindowTitle.setText("Create a Saver");
+        } else if (mCardType == MODE_SENDCARD) {
+            mWindowTitle.setText("Create a Sender");
+        } else if (mCardType == MODE_MONITORCARD) {
+            mWindowTitle.setText("Create a Monitor");
+        } else {
+            Log.e(TAG, "错误的卡片格式");
+            throw new CreateCardWindowException();
         }
-        //获取控件
-        mTitleEditText = (EditText) mContentView.findViewById(R.id.edit_title);
-        mSingleSelectLayout = (LinearLayout) mContentView.findViewById(R.id.edit_singleselect_layout);
-        mMultiSelectLayout = (LinearLayout) mContentView.findViewById(R.id.edit_multiselect_layout);
+        //卡片标题输入框
+        mTagEditText = (EditText) mContentView.findViewById(R.id.edit_title);
+        //单选模块or多选模块
+        View mSelectLayout = (View) mContentView.findViewById(R.id.edit_select_layout);
+        if (mCardType == MODE_MONITORCARD || mCardType == MODE_SENDCARD) {
+            mSelectAdapter = new SelectAdapter(mSelectLayout, SelectAdapter.MODE_SINGLE);
+        } else {
+            mSelectAdapter = new SelectAdapter(mSelectLayout, SelectAdapter.MODE_MULTI);
+        }
+        //TODO 刚才改到这个位置
+
+
+
         mParameterLayout = (View) mContentView.findViewById(R.id.edit_parameter_layout);
-        mSubmitBtn = (Button) mContentView.findViewById(R.id.edit_submit);
-        //提交按钮的事件
+        //提交按钮
+        Button mSubmitBtn = (Button) mContentView.findViewById(R.id.edit_submit);
         mSubmitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,7 +104,7 @@ public class CreateCardWindow {
                 //MonitorCard和SendCard都必须选一个通道
                 int channelInt = -1;
                 if (mCardType == MODE_MONITORCARD || mCardType == MODE_SENDCARD) {
-                    channelInt = mSingleSelectAdapter.getSelectedItemId();
+                    channelInt = mSelectAdapter.getSelectedItemId();
                     if (channelInt < 0 || channelInt >= WorkFlow.channelNumber) {
                         CreateCardIgnoreFragment ignoreFragment = new CreateCardIgnoreFragment("Please choose ONE channel.");
                         ignoreFragment.show(((Activity) mContext).getFragmentManager(), "？蛤？");
@@ -128,7 +123,7 @@ public class CreateCardWindow {
                 }
                 //SaveCard至少要选一个通道
                 if (mCardType == MODE_SAVECARD) {
-                    if (mMultiSelectAdapter.getChooseItemCound() == 0) {
+                    if (mSelectAdapter.getSelectedItemCount() == 0) {
                         CreateCardIgnoreFragment ignoreFragment = new CreateCardIgnoreFragment("Please choose AT LEAST ONE channel.");
                         ignoreFragment.show(((Activity) mContext).getFragmentManager(), "？蛤？");
                         return;
@@ -136,7 +131,7 @@ public class CreateCardWindow {
                 }
                 //发广播
                 Log.d(TAG, "发广播");
-                String title = mTitleEditText.getText().toString().trim();
+                String title = mTagEditText.getText().toString().trim();
                 String action = "";
                 if (mCardType == MODE_MONITORCARD) {
                     action = Action_CreateMonitorCard;
@@ -149,7 +144,7 @@ public class CreateCardWindow {
                 intent.putExtra("Id", channelInt);
                 intent.putExtra("Title", title);
                 if (mCardType == MODE_SAVECARD) {
-                    intent.putExtra("ChannelList", mMultiSelectAdapter.getStateInArray());
+                    intent.putExtra("ChannelList", mSelectAdapter.getStateInArray());
                 } else {
                     intent.putExtra("cha", channelInt);
                 }
@@ -159,7 +154,7 @@ public class CreateCardWindow {
             }
         });
         //关闭按钮事件：尝试关闭
-        mCloseBtn = (Button) mContentView.findViewById(R.id.edit_close);
+        Button mCloseBtn = (Button) mContentView.findViewById(R.id.edit_close);
         mCloseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -179,61 +174,10 @@ public class CreateCardWindow {
             public void onClick(View v) {
             }
         });
-        //通道名称数组
-        mOptionList = new ArrayList<>();
-        for (int i = 0; i < WorkFlow.channelNumber; i++) {
-            mOptionList.add("channel #" + i);
-        }
     }
 
-    //设置单选或多选
-    private void initSelectPart() {
-        if (mCardType == MODE_MONITORCARD || mCardType == MODE_SENDCARD) {
-            //可见性
-            mSingleSelectLayout.setVisibility(View.VISIBLE);
-            mMultiSelectLayout.setVisibility(View.GONE);
-            //单选模块
-            mSingleSelectRecView = (RecyclerView) mContentView.findViewById(R.id.edit_mo_choose_recview);
-            mSingleSelectLayoutManager = new LinearLayoutManager(mContext);
-            mSingleSelectLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
-            //mSingleSelectLayoutManager = new FullyLinearLayoutManager(mContext);
-            mSingleSelectRecView.setLayoutManager(mSingleSelectLayoutManager);
-            mSingleSelectAdapter = new CreateCardSingleSelectAdapter(mSingleSelectRecView, mOptionList);
-            mSingleSelectRecView.setAdapter(mSingleSelectAdapter);
-        } else {
-            //可见性
-            mSingleSelectLayout.setVisibility(View.GONE);
-            mMultiSelectLayout.setVisibility(View.VISIBLE);
-            //多选模块
-            mMultiSelectRecView = (RecyclerView) mContentView.findViewById(R.id.edit_sa_choose_recview);
-            mMultiSelectLayoutManager = new GridLayoutManager(mContext, 2, RecyclerView.HORIZONTAL, false);
-            mMultiSelectRecView.setLayoutManager(mMultiSelectLayoutManager);
-            mMultiSelectAdapter = new CreateCardMultiSelectAdapter(mOptionList);
-            mMultiSelectRecView.setAdapter(mMultiSelectAdapter);
-            //三个快捷操作按钮的事件
-            mChooseAllBtn = (Button) mContentView.findViewById(R.id.edit_sa_choose_all);
-            mChooseAllBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mMultiSelectAdapter.chooseAll();
-                }
-            });
-            mChooseNoneBtn = (Button) mContentView.findViewById(R.id.edit_sa_choose_none);
-            mChooseNoneBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mMultiSelectAdapter.chooseNone();
-                }
-            });
-            mChooseInvertBtn = (Button) mContentView.findViewById(R.id.edit_sa_choose_invert);
-            mChooseInvertBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mMultiSelectAdapter.chooseInvert();
-                }
-            });
-        }
-    }
+
+
 
     //SendCard专享部分
     private void initSendCard() {
@@ -253,20 +197,12 @@ public class CreateCardWindow {
     //检测弹窗是否没有包含任何信息
     private boolean isWindowEmpty() {
         //检测共享部分
-        if (mTitleEditText != null && mTitleEditText.getText().toString().trim().length() > 0) {
+        if (mTagEditText != null && mTagEditText.getText().toString().trim().length() > 0) {
             return false;
         }
-        //检测MonitorCard专享部分
-        if (mCardType == MODE_MONITORCARD) {
-            if (mSingleSelectAdapter != null && mSingleSelectAdapter.getSelectedItemId() != -1) {
-                return false;
-            }
-        }
-        //检测SaveCard专享部分
-        if (mCardType == MODE_SAVECARD) {
-            if (mMultiSelectAdapter != null && mMultiSelectAdapter.getChooseItemCound() > 0) {
-                return false;
-            }
+        //检测单选或多选
+        if (mSelectAdapter != null && !mSelectAdapter.isEmpty()) {
+            return false;
         }
         //检测SendCard专享部分
         if (mCardType == MODE_SENDCARD) {
@@ -292,5 +228,182 @@ public class CreateCardWindow {
         mContentView.findViewById(R.id.creat_card_window).setBackgroundDrawable(new BitmapDrawable(bmp2));
         //在屏幕中央显示
         mPopupWindow.showAtLocation(((Activity) mContext).getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+    }
+
+    //单选模块或多选模块的适配器
+    public static class SelectAdapter {
+        public static String TAG = "SelectViewHolderTag";
+
+        public static int MODE_SINGLE = 1;
+        public static int MODE_MULTI = 2;
+
+        private int mSelectMode;
+        private CheckBox mCheckBox[] = new CheckBox[4];
+        private int mSelectNow;     //这个变量单选模式才有用
+
+        public SelectAdapter(View selectView, int selectMode) {
+            mSelectMode = selectMode;
+            if (mSelectMode != MODE_SINGLE && mSelectMode != MODE_MULTI) {
+                Log.e(TAG, "错误的selectMode");
+                throw new SelectAdapterException();
+            }
+            mSelectNow = -1;
+            //获取每一个CheckBox控件
+            mCheckBox[0] = (CheckBox) selectView.findViewById(R.id.edit_select_0).findViewById(R.id.edit_select_checkbox);
+            mCheckBox[0].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    oneBoxChanged(0, isChecked);
+                }
+            });
+            mCheckBox[1] = (CheckBox) selectView.findViewById(R.id.edit_select_1).findViewById(R.id.edit_select_checkbox);
+            mCheckBox[1].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    oneBoxChanged(1, isChecked);
+                }
+            });
+            mCheckBox[2] = (CheckBox) selectView.findViewById(R.id.edit_select_2).findViewById(R.id.edit_select_checkbox);
+            mCheckBox[2].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    oneBoxChanged(2, isChecked);
+                }
+            });
+            mCheckBox[3] = (CheckBox) selectView.findViewById(R.id.edit_select_3).findViewById(R.id.edit_select_checkbox);
+            mCheckBox[3].setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    oneBoxChanged(3, isChecked);
+                }
+            });
+            //给每个通道选项改名字
+            TextView mChannelName;
+            mChannelName = (TextView) selectView.findViewById(R.id.edit_select_0).findViewById(R.id.edit_select_channelname);
+            mChannelName.setText("#0");
+            mChannelName = (TextView) selectView.findViewById(R.id.edit_select_1).findViewById(R.id.edit_select_channelname);
+            mChannelName.setText("#1");
+            mChannelName = (TextView) selectView.findViewById(R.id.edit_select_2).findViewById(R.id.edit_select_channelname);
+            mChannelName.setText("#2");
+            mChannelName = (TextView) selectView.findViewById(R.id.edit_select_3).findViewById(R.id.edit_select_channelname);
+            mChannelName.setText("#3");
+            //处理介绍文字
+            TextView mDescription = (TextView) selectView.findViewById(R.id.edit_select_description);
+            if (mSelectMode == MODE_SINGLE) {
+                mDescription.setText("Choose one channel:");
+            } else {
+                mDescription.setText("Choose channel(s) to save:");
+            }
+            //处理三个快速选择按钮
+            View mBtns = (View) selectView.findViewById(R.id.edit_select_btns);
+            if (mSelectMode == MODE_SINGLE) {
+                mBtns.setVisibility(View.GONE);
+            } else {
+                Button mAll = (Button) selectView.findViewById(R.id.edit_select_all);
+                mAll.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectAll();
+                    }
+                });
+                Button mNone = (Button) selectView.findViewById(R.id.edit_select_none);
+                mNone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectNone();
+                    }
+                });
+                Button mOther = (Button) selectView.findViewById(R.id.edit_select_others);
+                mOther.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        selectOthers();
+                    }
+                });
+            }
+        }
+
+        private void oneBoxChanged(int id, boolean isChecked) {
+            Log.d(TAG, "oneBoxChanged: id = " + id + " isChecked = " + isChecked);
+            Log.d(TAG, "oneBoxChanged: mSelectMode = " + mSelectMode + " mSelectNow = " + mSelectNow);
+            if (mSelectMode == MODE_SINGLE) {
+                if (isChecked) {
+                    if (0 <= mSelectNow && mSelectNow <= 3) {
+                        mCheckBox[mSelectNow].setChecked(false);
+                    }
+                    mSelectNow = id;
+                } else {
+                    mSelectNow = -1;
+                }
+            }
+        }
+        private void selectAll() {
+            for (int i = 0; i < 4; i++) {
+                mCheckBox[i].setChecked(true);
+            }
+        }
+        private void selectNone() {
+            for (int i = 0; i < 4; i++) {
+                mCheckBox[i].setChecked(false);
+            }
+        }
+        private void selectOthers() {
+            for (int i = 0; i < 4; i++) {
+                mCheckBox[i].setChecked(!mCheckBox[i].isChecked());
+            }
+        }
+
+        public int getSelectedItemId() {
+            if (mSelectMode == MODE_MULTI) {
+                throw new SelectAdapterException();
+            } else {
+                return mSelectNow;
+            }
+        }
+        public int getSelectedItemCount() {
+            if (mSelectMode == MODE_SINGLE) {
+                throw new SelectAdapterException();
+            } else {
+                int cnt = 0;
+                for (int i = 0; i < 4; i++) {
+                    if (mCheckBox[i].isChecked()) {
+                        cnt++;
+                    }
+                }
+                return cnt;
+            }
+        }
+        public int[] getStateInArray() {
+            if (mSelectMode == MODE_SINGLE) {
+                throw new SelectAdapterException();
+            } else {
+                int radio[] = new int[4];
+                for (int i = 0; i < 4; i++) {
+                    if (mCheckBox[i].isChecked()) {
+                        radio[i] = 1;
+                    } else {
+                        radio[i] = 0;
+                    }
+                }
+                return radio;
+            }
+        }
+        public boolean isEmpty() {
+            if (mSelectMode == MODE_SINGLE) {
+                return mSelectNow == -1;
+            } else {
+                for (int i = 0; i < 4; i++) {
+                    if (mCheckBox[i].isChecked()) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+        public static class SelectAdapterException extends RuntimeException {
+        }
+    }
+    public static class CreateCardWindowException extends RuntimeException {
     }
 }
