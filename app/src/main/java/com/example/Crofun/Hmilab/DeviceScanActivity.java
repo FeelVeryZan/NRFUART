51e0f4ec61170f300543b8bda11d9a924e532d45
CudraniaE <li_zhe-yi@163.com>
2018-04-13 11:28:53 +0800

    暂时性commit，并没有真正解决蓝牙问题。

---
+++ app/src/main/java/com/example/Crofun/Hmilab/DeviceScanActivity.java
@@ -0,0 +1,50 @@
+package com.example.Crofun.Hmilab;
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Handler;
+
+/**
+ * Created by Angel on 2018/4/13.
+ */
+
+public class DeviceScanActivity extends BaseActivity {
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
